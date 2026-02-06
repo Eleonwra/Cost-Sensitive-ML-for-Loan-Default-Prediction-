90702dd9d8c24ad59e7f3e7a7b3d67b2e7f8e3db
Stoikopoulou Eleonora <stoikopoyloyeleonwra@gmail.com>
2026-02-06 14:44:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,153 @@
+#Standard libraries
+import os
+
+#Data Handling
+import pandas as pd
+import numpy as np
+
+#Preprocessing
+from sklearn.datasets import  fetch_openml
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer, make_column_selector
+
+#Machine learning models
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+#Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from src.evaluation import sampling_evaluate, weights_evaluate, calibration_evaluate
+
+def main():
+    os.makedirs("plots", exist_ok=True)
+    
+    X, y = fetch_openml("credit-g", version=1, as_frame=True, parser='auto', return_X_y=True)
+    cost_m = [[0, 1], [5, 0]]
+    mapping = {'good': 0, 'bad': 1}
+
+    one_hot_encoder = make_column_transformer(
+    (OneHotEncoder(sparse_output=False, handle_unknown='ignore'),
+     make_column_selector(dtype_include='category')),
+    remainder='passthrough')
+
+    X = one_hot_encoder.fit_transform(X)
+    X = pd.DataFrame(X)
+
+    sampling_strategies = [ ('baseline', None), ('hybrid_cost_ratio', {'majority': 0.5, 'minority': 5.0})]
+    names = ['Random Forest', 'Linear SVM', 'Gaussian NB']
+    classifiers = [
+        RandomForestClassifier(n_estimators=100, random_state=42),
+        SVC(kernel='linear'),
+        GaussianNB()]
+    
+    final_comparison_data = []
+
+    print("\n--- Running Resampling-Based Evaluation ---")
+    for sampler_name, sampler in sampling_strategies:
+        print(f"\n=== {sampler_name} ===")
+        results, fold_matrices = sampling_evaluate(X, y, classifiers, names, cost_m, sampler)
+        for name in names:
+            final_comparison_data.append({
+            "Technique": sampler_name,
+            "Model": name,
+            "Mean Loss": results[name]["loss"],
+            "Std Loss": results[name]["std"],
+            "Mean Acc": results[name]["accuracy"]})
+
+    print("\n--- Running Weight-Based Evaluation ---\n")
+
+    results, fold_matrices = weights_evaluate(X, y, classifiers, names, cost_m, weight_bad=5, weight_good=1)
+    for name in names:
+        final_comparison_data.append({
+            "Technique": 'weight_based',
+            "Model": name,
+            "Mean Loss": results[name]["loss"],
+            "Std Loss": results[name]["std"],
+            "Mean Acc": results[name]["accuracy"]})
+    
+    print("\n--- Running Calibration-Based Evaluation ---")
+    classifiers = [
+        RandomForestClassifier(n_estimators=100, random_state=42),
+        SVC(kernel='linear', probability=True),
+        GaussianNB()
+    ]
+    calibrations = ['sigmoid', 'isotonic']
+    for cal in calibrations:
+        results, fold_matrices = calibration_evaluate(X, y, mapping, classifiers, names, cost_m, calibrate=cal)
+        for name in names:
+            final_comparison_data.append({
+            "Technique": cal,
+            "Model": name,
+            "Mean Loss": results[name]["loss"],
+            "Std Loss": results[name]["std"],
+            "Mean Acc": results[name]["accuracy"]})
+
+    print("\n--- Final Comparison Table ---")
+
+    final_df = pd.DataFrame(final_comparison_data)
+    final_df = final_df.sort_values(by="Mean Loss")
+
+    print("\n" + "="*70)
+    print("FINAL CROSS-VALIDATION PERFORMANCE LEADERBOARD")
+    print("="*70)
+    print(final_df.to_string(index=False))
+
+    final_df.to_csv("final_leaderboard.csv", index=False)
+
+    final_df['Full_Name'] = final_df['Technique'] + " - " + final_df['Model']
+    final_df = final_df.sort_values('Mean Loss')
+
+    plt.figure(figsize=(14, 8))
+
+    bars = plt.bar(
+        x=final_df['Full_Name'], 
+        height=final_df['Mean Loss'], 
+        yerr=final_df['Std Loss'], 
+        capsize=5, 
+        color=plt.cm.viridis(np.linspace(0, 1, len(final_df))),
+        edgecolor='black',
+        alpha=0.8
+    )
+
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2., height + 5,
+                f'{height:.0f}', ha='center', va='bottom', fontweight='bold')
+
+    plt.xticks(rotation=90)
+    plt.title("Global Cost leaderboard", fontsize=15)
+    plt.ylabel("Mean Loss")
+    plt.grid(axis='y', linestyle='--', alpha=0.3)
+
+    plt.tight_layout()
+    plt.savefig("plots/Global_Cost_Leaderboard.png")
+    plt.close()
+
+    plt.figure(figsize=(12, 7))
+    ax = sns.barplot(
+        data=final_df, 
+        x='Model', 
+        y='Mean Loss', 
+        hue='Technique',
+        capsize=.1,
+        errorbar=None 
+    )
+
+    for container in ax.containers:
+        ax.bar_label(container, fmt='%.0f', padding=3, fontsize=9)
+
+    plt.title("Strategy perfomance grouped by Classifier", fontsize=14, pad=20)
+    plt.ylabel("Total Expected Loss", fontsize=12)
+    plt.xlabel("Classifier", fontsize=12)
+    plt.legend(title="Optimization Technique", bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.savefig("plots/Strategy_Performance_by_Classifier.png")
+    plt.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/evaluation.py
@@ -0,0 +1,221 @@
+# Data Handling
+import numpy as np
+from collections import Counter
+
+# Model Utilities
+from sklearn.base import clone
+from sklearn.pipeline import Pipeline
+
+# Preprocessing & Validation
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.preprocessing import StandardScaler
+
+#Sampling
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+
+# Evaluation Metrics
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+
+def sampling_evaluate(X, y, classifiers, names, cost_matrix, sampler=None, splits=5):
+    """
+    Evaluates classifiers using a specific sampling strategy and a cost matrix.
+    
+    Args:
+        X: Feature DataFrame
+        y: Target Series
+        classifiers: A list of scikit-learn classifier objects
+        names: The string names for the classifiers for reporting
+        cost_matrix: 2x2 array defining costs of FP and FN.
+        sampler: An imblearn sampler object
+        splits: Number of folds for StratifiedKFold.
+    Returns:
+        final_summary: Dictionary of model performance.
+        fold_matrices: Aggregated confusion matrices.
+    """
+    
+    skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=42)
+
+    fold_loss = {name: [] for name in names} 
+    fold_matrices = {name: np.zeros((2, 2)) for name in names}
+    fold_accuracies = {name: [] for name in names}
+    final_summary = {}
+
+    for train_idx, test_idx in skf.split(X, y):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+        
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+        
+        if sampler is not None:
+            counts = Counter(y_train)
+            majority_class = max(counts, key=counts.get) 
+            minority_class = min(counts, key=counts.get) 
+    
+            n_maj = counts[majority_class]
+            n_min = counts[minority_class]
+            
+            target_maj = int(n_maj * sampler['majority']) 
+            target_min = int(n_min * sampler['minority'])
+
+            rus = RandomUnderSampler(sampling_strategy={'good': target_maj, 'bad': n_min}, random_state=42)
+            X_mid, y_mid = rus.fit_resample(X_train_scaled, y_train)
+            
+            ros = RandomOverSampler(sampling_strategy={'good': target_maj, 'bad': target_min}, random_state=42)
+            X_res, y_res = ros.fit_resample(X_mid, y_mid)
+        else:
+            X_res, y_res = X_train_scaled, y_train
+
+        for name, clf in zip(names, classifiers):
+            model = clone(clf)
+            model.fit(X_res, y_res)
+            y_pred = model.predict(X_test_scaled)
+            conf_m = confusion_matrix(y_test, y_pred, labels = ['good', 'bad'])
+            fold_matrices[name] += conf_m
+ 
+            loss = np.sum(conf_m * cost_matrix) 
+            fold_loss[name].append(loss)
+
+            acc = accuracy_score(y_test, y_pred)
+            fold_accuracies[name].append(acc)
+    
+    for name in names:
+        avg_loss = np.mean(fold_loss[name])
+        std_loss = np.std(fold_loss[name])
+        avg_acc = np.mean(fold_accuracies[name])
+        final_summary[name] = {"loss": avg_loss, "accuracy": avg_acc, "std": std_loss}
+        print(f"{name} → Average loss: {avg_loss:.2f} | Accuracy: {avg_acc:.2%}")
+    return final_summary, fold_matrices
+
+def weights_evaluate(X, y, classifiers, names, cost_matrix, weight_bad=5, weight_good=1, splits = 5):
+    """
+    Evaluates classifiers using sample weights during training.
+
+    Args:
+        X: Feature DataFrame
+        y: Target Series
+        classifiers: A list of scikit-learn classifier objects
+        names: The string names for the classifiers for reporting
+        cost_matrix: 2x2 array defining costs of FP and FN.
+        weight_bad: Penalty multiplier applied to samples labeled 'bad'
+        weight_good: Penalty multiplier applied to samples labeled 'good'
+        splits: Number of folds for StratifiedKFold.
+    Returns:
+        final_summary: Dictionary of model performance.
+        fold_matrices: Aggregated confusion matrices.
+
+    """
+    skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=42)
+
+    fold_loss = {name: [] for name in names} 
+    fold_matrices = {name: np.zeros((2, 2)) for name in names}
+    fold_accuracies = {name: [] for name in names}
+    final_summary = {}
+
+    for train_idx, test_idx in skf.split(X, y):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+        
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+
+        weights = np.ones(len(y_train))
+
+        weights[y_train == 'bad'] = weight_bad
+        weights[y_train == 'good'] = weight_good
+
+        for name, clf in zip(names, classifiers):
+            model = clone(clf)
+            model.fit(X_train_scaled, y_train, sample_weight=weights)
+            y_pred = model.predict(X_test_scaled)
+            conf_m = confusion_matrix(y_test, y_pred, labels = ['good','bad'])
+            fold_matrices[name] += conf_m
+            loss = np.sum(conf_m * cost_matrix)
+            fold_loss[name].append(loss)
+
+            acc = accuracy_score(y_test, y_pred)
+            fold_accuracies[name].append(acc)
+
+    for name in names:
+        avg_loss = np.mean(fold_loss[name])
+        std_loss = np.std(fold_loss[name])
+        avg_acc = np.mean(fold_accuracies[name])
+        final_summary[name] = {"loss": avg_loss, "accuracy": avg_acc, "std": std_loss}
+        print(f"{name} → Average loss: {avg_loss:.2f} | Accuracy: {avg_acc:.2%}")
+    return final_summary, fold_matrices
+
+def calibration_evaluate(X, y, mapping, clfs, names, cost_matrix, calibrate=None, n_splits=5):
+    """
+    Evaluates classifiers using probability calibration and Bayes Risk Minimization.
+    
+    This method optimizes the decision threshold by calculating the expected cost 
+    for each class. Instead of predicting the class with the highest probability, 
+    it predicts the class that minimizes total financial risk based on the cost_matrix.
+
+    Args:
+        X: Feature DataFrame
+        y: Target Series
+        mapping: Dictionary to map original labels to integers
+        clfs: A list of scikit-learn classifier objects
+        names: The string names for the classifiers for reporting
+        cost_matrix: 2x2 array defining costs of FP and FN.
+        calibrate: Calibration method
+        n_splits: Number of folds for StratifiedKFold.
+    Returns:
+        final_summary: Dictionary of model performance.
+        fold_matrices: Aggregated confusion matrices.
+    """
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+    fold_loss = {name: [] for name in names} 
+    fold_matrices = {name: np.zeros((2, 2)) for name in names}
+    fold_accuracies = {name: [] for name in names}
+    final_summary = {}
+
+    for train_idx, test_idx in skf.split(X, y):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+        
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+        
+        y_train = np.array([mapping[val] for val in y_train])
+        y_test  = np.array([mapping[val] for val in y_test])
+    
+        for name, clf in zip(names, clfs):
+            base_clf = clone(clf)
+            if calibrate is None:
+                model = base_clf.fit(X_train_scaled, y_train)
+            else:
+                model = CalibratedClassifierCV(
+                    base_clf,
+                    method=calibrate,
+                    cv=3
+                ).fit(X_train_scaled, y_train)
+            y_pred_prob = model.predict_proba(X_test_scaled)
+            y_pred = np.argmin(
+                np.matmul(y_pred_prob, np.array(cost_matrix)),
+                axis=1)
+            conf_m = confusion_matrix(
+                y_test, y_pred, labels = [0, 1])
+            fold_matrices[name] += conf_m
+            loss = np.sum(conf_m * cost_matrix)
+            fold_loss[name].append(loss)
+
+            acc = accuracy_score(y_test, y_pred)
+            fold_accuracies[name].append(acc)
+
+    print(f"\n=== Calibration: {calibrate if calibrate else 'none'} ===")
+    for name in names:
+        avg_loss = np.mean(fold_loss[name])
+        std_loss = np.std(fold_loss[name])
+        avg_acc = np.mean(fold_accuracies[name])
+        final_summary[name] = {"loss": avg_loss, "accuracy": avg_acc, "std": std_loss}
+        print(f"{name} → Average loss: {avg_loss:.2f} | Accuracy: {avg_acc:.2%}")
+    return final_summary, fold_matrices
